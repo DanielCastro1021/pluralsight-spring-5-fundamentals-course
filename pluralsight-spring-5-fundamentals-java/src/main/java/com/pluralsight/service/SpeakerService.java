9d2610dc3166ceec79233df1caba353df4532ae5
Daniel Castro <danielcastrofafe@gmail.com>
2022-06-20 17:55:02 +0100

    @PostConstruct Example

--- pluralsight-spring-5-fundamentals-java/src/main/java/com/pluralsight/service/SpeakerService.java
+++ pluralsight-spring-5-fundamentals-java/src/main/java/com/pluralsight/service/SpeakerService.java
@@ -5,21 +5,27 @@ import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
 public class SpeakerService implements ISpeakerService {
-
+    @Autowired
     private SpeakerRepository repository;
 
     public SpeakerService() {
         System.out.println("SpeakService no args constructor");
     }
 
-    @Autowired
+
     public SpeakerService(SpeakerRepository repository) {
         System.out.println("SpeakService repository constructor");
         this.repository = repository;
+    }
+
+    @PostConstruct
+    private void init() {
+        System.out.println("We're called after constructors");
     }
 
     @Override
